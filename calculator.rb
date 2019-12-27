f54d88d5b618b55f84bf0447034ce517c2a449e2
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2019-12-27 21:18:43 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,9 @@
 # Add your variables here
 first_number = 10 
 second_number = 10
+describe 
+it first_number = get_variable_from_file('./calculator.rb',"first_number")
+get_variable_from_file('./calculator.rb',"first_number")
 
+expect(first_number).to be_an(Integer).or be_a(Float)
 
